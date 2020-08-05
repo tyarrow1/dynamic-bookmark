dc38aea62f5f076c796f2d3cd41c02d75ad63bf6
Tom Yarrow <tyarrow1@gmail.com>
2020-08-05 12:40:13 -0500

    began bookmark functionality, fixed other button

--- scripts/init.js
+++ scripts/init.js
@@ -4,19 +4,25 @@ var bookmark_pairs = [];
 var bookmark_mode = false;
 var parent_folder = null;
 
+// Shortcuts for chrome things
+var storage = chrome.storage.sync;
+var bookmarks = chrome.bookmarks;
+
 // Intialize event listeners (to avoid inline-scripts)
 document.getElementById("folder_name_submit").onclick = change_folder_name;
+document.getElementById("bookmark_update").onclick = set_bookmark;
+document.getElementById("bookmark_delete").onclick = delete_bookmark;
+document.getElementById("bookmark_create").onclick = create_bookmark;
 
-// Run init() (which can be run multiple times)
+// Run init() upon popup load (which can be run multiple times)
 init();
 
+// CORE FUNCTIONS
+//---------------------------------------------------------------------
 // This function is the initial function, which can be run anytime to refresh the bookmarks/folder
 async function init() {
-    // Shorcut to reduce function call complexity
-    let storage = chrome.storage.sync;
-
     // Obtain the folder name from chrome storage
-    let name = await promise_wrapper(storage.get, storage, ['DYBK_folder_name']);
+    let name = await promise_wrapper(storage.get, storage, 'DYBK_folder_name');
     if (typeof(name) === "undefined") {
         print("Folder not found");
         bookmark_mode = false;
@@ -32,9 +38,6 @@ async function init() {
 
 // This function will find the bookmark folder, creating it if it needs to
 async function search_create_folder() {
-    // Shortcut to reduce function call complexity
-    let bookmarks = chrome.bookmarks;
-
     // Get the ID of the "Other Bookmarks" folder
     let otherBookmarksID = (await promise_wrapper(bookmarks.getTree, bookmarks))[0].children[1].id;
 
@@ -42,11 +45,11 @@ async function search_create_folder() {
     let search_term = {
         'title': folder_name
     };
-    let searchResults = (await promise_wrapper(bookmarks.search, bookmarks, [search_term]));
+    let searchResults = (await promise_wrapper(bookmarks.search, bookmarks, search_term));
     if (typeof (searchResults) === "undefined" || searchResults.length === 0) {
         // Create the folder
         print("Results not found, making folder");
-        create_folder();
+        await create_folder(otherBookmarksID);
     } else {
         print("Results found, verifying");
 
@@ -61,7 +64,7 @@ async function search_create_folder() {
         if (target === null) {
             // A bookmark with the same name as the folder is found, but it isn't a folder in the proper section
             print("Results incorrect, making folder");
-            create_folder();
+            await create_folder(otherBookmarksID);
         } else {
             parent_folder = target;
         }
@@ -70,16 +73,13 @@ async function search_create_folder() {
 }
 
 // This function creates the bookmark folder (called by search_create_folder)
-async function create_folder() {
-    // Shortcut to reduce function call complexity
-    let bookmarks = chrome.bookmarks;
-
+async function create_folder(parentId) {
     // The two properties of the new folder that matter
     let new_folder_props = {
-        'parentId': otherBookmarksID,
+        'parentId': parentId,
         'title': folder_name
     };
-    let created_folder = await promise_wrapper(bookmarks.create, bookmarks, [new_folder_props]);
+    let created_folder = await promise_wrapper(bookmarks.create, bookmarks, new_folder_props);
     parent_folder = created_folder;
     print("Created " + parent_folder.title + " bookmark folder");
 }
@@ -88,8 +88,7 @@ async function create_folder() {
 async function populate_bookmarks() {
     print("populating bookmarks");
 
-    let bookmarks = chrome.bookmarks;
-    let results = await promise_wrapper(bookmarks.getChildren,bookmarks,[parent_folder.id]);
+    let results = await promise_wrapper(bookmarks.getChildren,bookmarks,parent_folder.id);
     if (typeof (results) === "undefined" || results.length === 0) {
         print("No bookmarks found in folder");
         bookmark_pairs = [];
@@ -117,6 +116,8 @@ async function update_bookmark_dropdown() {
     // Use a variable as a shortcut
     let dropdown = document.getElementById("bookmark_selection");
 
+    // TODO: Alphabetically sort bookmark entries in the dropdown, place the current URL's bookmark on top of the list
+
     // Fill the table with an invalid option if the folder must be created
     if (bookmark_mode = false) {
         dropdown.innerHTML = '<option value="no_bookmarks">--</option>\n';
@@ -126,7 +127,10 @@ async function update_bookmark_dropdown() {
         bookmark_pairs.forEach((element) => {
             dropdown_contents += `<option value="${element.title}">${element.title}</option>`;
         });
-        dropdown_contents += '<option value="new_bookmark">Create a New Bookmark</option>';
+        if (dropdown_contents === '') {
+            dropdown_contents += '<option value="new_bookmark">Create a New Bookmark</option>';
+        }
+        
         dropdown.innerHTML = dropdown_contents;
     }
 }
@@ -135,37 +139,108 @@ function move_folders() {
 
 }
 
-function change_folder_name() {
+
+// BUTTON FUNCTIONS
+//---------------------------------------------------------------------
+// Function for setting the bookmark (activated via the "update" button)
+async function set_bookmark() {
+    let bookmark_name = document.getElementById("bookmark_selection").value;
+
+    // TODO: more bookmark name verification
+
+    if (bookmark_name === "new_bookmark") {
+        print("Can't update the 'create bookmark' option");
+        return;
+    }
+
+    let new_bookmark_url = await current_url();
+
+    // Next, update the bookmark's url
+}
+
+// Function for removing a bookmark (activated via the "delete" button)
+async function delete_bookmark() {
+    let bookmark_name = document.getElementById("bookmark_selection").value;
+
+    // TODO: more bookmark name verification
+
+    if (bookmark_name === "new_bookmark") {
+        print("Can't delete the 'create bookmark' option");
+        return;
+    }
+
+    // Next, delete the bookmark
+}
+
+// Function for creating a new bookmark (activated via the "create" button)
+async function create_bookmark() {
+    let bookmark_name = document.getElementById("bookmark_name_field").value;
+    
+    // TODO: Input verify to make sure that it doesn't overwrite any existing bookmarks
+
+    if (bookmark_name === "") {
+        print("Bookmark has no name");
+        return;
+    }
+    let bookmark_url = await current_url();
+
+    let new_bookmark = {
+        "parentId": parent_folder.id,
+        "title": bookmark_name,
+        "url": bookmark_url
+    };
+
+    print(`Creating ${bookmark_name}:${bookmark_url}`);
+
+    await promise_wrapper(bookmarks.create,bookmarks,new_bookmark);
+
+    await populate_bookmarks();
+}
+
+// Function for changing the folder that the bookmarks are stored in
+async function change_folder_name() {
     // Get the folder name and (minimally) verify it
-    var new_name = document.getElementById("folder_name_field").value;
+    let new_name = document.getElementById("folder_name_field").value;
     if (new_name === "") {
         return;
     }
 
     print("New Name: " + new_name + "\n");
 
-    chrome.storage.sync.set({ 'DYBK_folder_name': new_name }, function (result) {
-        // Move existing bookmarks if there is a current folder
-        if (folder_name !== "") {
-            print("Bookmarks should be moved\n");
-        }
+    await promise_wrapper(storage.set,storage,{ 'DYBK_folder_name': new_name });
+    // Move existing bookmarks if there is a current folder
+    if (folder_name !== "") {
+        print("Bookmarks should be moved\n");
+    }
 
-        // Update the folder_name variable (although it will already be done in init())
-        folder_name = new_name;
+    // Update the folder_name variable (although it will already be done in init())
+    folder_name = new_name;
 
-        // Re-init to make the extension open the new folder
-        init();
-    });
+    // Re-init to make the extension open the new folder
+    init();
 }
 
+
+// UTILITY FUNCTIONS
+//---------------------------------------------------------------------
 // Utility function to allow async/await to be used on the chrome functions (thus avoiding callback hell)
 function promise_wrapper(func, context, options = []) {
     let newFunc = func.bind(context);
+    if (!Array.isArray(options)) {
+        options = [options];
+    }
     return new Promise(
         (resolve) => {
             newFunc(...options, resolve);
         }
     );
+}
+
+// Utility function to get the current tab's url
+async function current_url() {
+    let tabs = await promise_wrapper(chrome.tabs.query,chrome.tabs,{"active":true,"windowId":chrome.windows.WINDOW_ID_CURRENT});
+    print(tabs);
+    return tabs[0].url;
 }
 
 // Utility function to print to the background script
